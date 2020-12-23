3af7d38a309df33c93318b6d6a8c8296126080fc
Vladislav <vlad1997kov@gmail.com>
2020-12-24 03:24:40 +0700

    setup migrations for tests, initial dao tests (#10)
    Co-authored-by: vladislav <vlad.kovalenko@aviasales.ru>

--- src/test/scala/scalajobs/dao/OrganizationDaoSpec.scala
+++ src/test/scala/scalajobs/dao/OrganizationDaoSpec.scala
@@ -1,21 +1,24 @@
 package scalajobs.dao
 
 import org.testcontainers.containers.PostgreSQLContainer
+import scalajobs.Migrations
 import scalajobs.configuration.{DbConfig, DbConnection}
 import scalajobs.model.Organization
+import scalajobs.model.dbParams.OrganizationDbParams
 import zio.{Has, UIO, URLayer, ZIO, ZLayer}
 import zio.test.{DefaultRunnableSpec, Gen, checkM, suite, testM}
 import zio.blocking.{Blocking, effectBlocking}
 import zio.logging.slf4j.Slf4jLogger
+import zio.random.Random
 import zio.test.Assertion._
 import zio.test.TestAspect._
-import zio.test.environment.{TestClock, TestEnvironment}
 import zio.test._
 import zio.test.magnolia._
+import zio.test.Gen.anyString
 
 object OrganizationDaoSpec extends DefaultRunnableSpec {
   val container = ZLayer.fromAcquireRelease(effectBlocking {
-    val cont = new PostgreSQLContainer("12")
+    val cont = new PostgreSQLContainer("postgres:12")
     cont.start()
     cont
   }.orDie)(cont => effectBlocking(cont.stop()).orDie)
@@ -34,16 +37,39 @@ object OrganizationDaoSpec extends DefaultRunnableSpec {
   val env =
     ZLayer.requires[Blocking] >+>
       container >+>
-      config >>>
+      config >+>
+      Migrations.live >+>
+      Migrations.afterMigrations >+>
       DbConnection.transactorLive >>>
       (OrganizationDao.live)
 
   val genOrganization = DeriveGen[Organization]
+  val genOrgDbParams: Gen[Random with Sized, OrganizationDbParams] =
+    anyString.noShrink.map(OrganizationDbParams)
 
   def spec =
-    suite("OrganizationDaoSpec")(testM("list returns list of organizations") {
-      checkM(Gen.vectorOf(genOrganization)) { users =>
-        ???
-      }
-    })
+    suite("OrganizationDaoSpec")(
+      testM("create ") {
+        checkM(Gen.vectorOf(genOrgDbParams)) { orgsParams =>
+          val names = for {
+            organizations <- ZIO.foreach(orgsParams)(OrganizationDao.create)
+          } yield organizations.map(_.get.name)
+
+          assertM(names)(hasSameElements(orgsParams.map(_.name)))
+        }
+      },
+      suite("get")(testM("existing org") {
+        checkM(Gen.vectorOfBounded(1, 11)(genOrgDbParams)) { orgsParams =>
+          for {
+            someOrgs <- ZIO.foreach(orgsParams)(OrganizationDao.create)
+            headOrg = someOrgs.head.get
+            res <- assertM(OrganizationDao.get(headOrg.id.get))(
+              isSome(equalTo(headOrg))
+            )
+          } yield res
+        }
+      })
+    ).@@(after(OrganizationDao.deleteAll))
+      .provideSomeLayerShared[Environment](env.orDie)
+
 }
